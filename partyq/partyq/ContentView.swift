72e407ee4f868c6c89adb5c3c45f6c7126a6b304
niehusst <liamniehusstaab@gmail.com>
2022-09-22 21:26:29 -0600

    chore: xcode skeleton setup

---
+++ partyq/partyq/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  partyq
+//
+//  Created by Liam1 on 9/22/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ partyq/partyq/partyqApp.swift
@@ -0,0 +1,17 @@
+//
+//  partyqApp.swift
+//  partyq
+//
+//  Created by Liam1 on 9/22/22.
+//
+
+import SwiftUI
+
+@main
+struct partyqApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
