78f1474ae274f35f9097819d5f6d344bc5600961
niehusst <liamniehusstaab@gmail.com>
2022-09-28 21:22:11 -0600

    ci: add pr checks

---
+++ partyq/partyq/Shared/Views/PartyqButton.swift
@@ -0,0 +1,20 @@
+//
+//  PartyqButton.swift
+//  partyq
+//
+//  Created by Liam1 on 9/28/22.
+//
+
+import SwiftUI
+
+struct PartyqButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PartyqButton_Previews: PreviewProvider {
+    static var previews: some View {
+        PartyqButton()
+    }
+}
