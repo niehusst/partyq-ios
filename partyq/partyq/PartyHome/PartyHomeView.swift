5e16613ebf2f9372bd43032a98fba4435b8c6fbb
niehusst <liamniehusstaab@gmail.com>
2022-10-15 22:39:25 -0600

    chore: add some empty views as placeholders

---
+++ partyq/partyq/PartyHome/PartyHomeView.swift
@@ -0,0 +1,20 @@
+//
+//  PartyHomeView.swift
+//  partyq
+//
+//  Created by Liam1 on 10/15/22.
+//
+
+import SwiftUI
+
+struct PartyHomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PartyHomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        PartyHomeView()
+    }
+}
---
+++ partyq/partyq/Shared/Views/InfoSheets/AboutSheet.swift
@@ -0,0 +1,20 @@
+//
+//  AboutSheet.swift
+//  partyq
+//
+//  Created by Liam1 on 10/15/22.
+//
+
+import SwiftUI
+
+struct AboutSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AboutSheet_Previews: PreviewProvider {
+    static var previews: some View {
+        AboutSheet()
+    }
+}
